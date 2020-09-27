9f7e8f0a97382c44dbc7da319ece81a293303c91
shorygina <teshorygina@edu.hse.ru>
2020-09-27 21:11:17 +0300

    Added courses table on main page

--- src/views/tasks-constructor/index.vue
+++ src/views/tasks-constructor/index.vue
@@ -1,73 +1,28 @@
 <template>
-  <div class="wraper">
-    <div class="navigation">
-      <section class="container">
-        <a
-          class="navigation-title"
-          href="/enter"
-          title="База КИМов - Сервис для преподавателей МИЭМ НИУ ВШЭ"
-        >
+  <div class="wrapper">
           <h1 class="title">База заданий</h1>
-        </a>
-      </section>
-    </div>
     <el-button class="buttonCreateGrader" type="primary" @click="handleClickCreateGrader()">Создать грейдер</el-button>
     <el-button class="buttonPublishGrader" type="primary" @click="handleClickPublishGrader()">Опубликовать грейдер</el-button>
     <el-button class="buttonCreateVariants" type="primary" @click="handleClickCreateVariants()">Создать варианты для КИМа</el-button>
     <el-button class="buttonGiveOutVariants" type="primary" @click="handleClickGiveOutVariants()">Раздать варианты для КИМа</el-button>
-    <el-table v-if="cmms.length > 0" ref="singleTable" :data="cmms" style="margin: 7%" @cell-click="handle">
+    <el-table v-if="this.courses.length > 0" ref="singleTable" :data="courses" style="margin: 7%" @cell-click="handle">
       <el-table-column
-        prop="spreadsheetName"
-        label="Имя КИМа"
-        width="300"
+        prop="name"
+        label="Название курса"
+        width="250"
       />
       <el-table-column
-        prop="spreadsheetUrl"
-        label="Сслыка на таблицу"
-        width="450"
+        prop="alternate_link"
+        label="Ссылка на курс"
+        width="200"
       />
+      <!--el-table-column
+        prop="???"
+        label="Число учащихся"
+        width="100"
+      /-->
     </el-table>
-    <section class="container" v-if="chosen" id="base">
-      <h4>Заполните данные для генерации вариантов из КИМа "{{ currentCmm }}" </h4>
-      <div v-if="spreadsheetInfo">
-        <el-form>
-          <p>Укажите количество вопросов из каждого раздела</p>
-          <el-form v-for="info in spreadsheetInfo" :key="info.title">
-            <label for="info.title"><p>{{ info.title }}</p></label>
-            <el-select ref="selectTopics" name="info.title" v-model="info.value" id="info.title">
-              <el-option :key="0" :value="''"></el-option>
-              <el-option v-for="i in info.amount" :value="i" :key="i">
-                {{ i }}
-              </el-option>
-            </el-select>
-          </el-form>
-          <el-form style="margin-top: 1rem">
-            <label for="amountOfVarients"><p>Укажите количество вариантов</p></label>
-            <el-input
-              v-model="amountOfVariants"
-              type="number"
-              placeholder="For example, 10"
-              name="amountOfVariants"
-              id="amountOfVariants"
-              min="1">
-            </el-input>
-          </el-form>
-          <el-form-item>
-            <div style="margin-top: 1rem">
-              <el-button
-                type="primary"
-                id="submit"
-                native-type="submit"
-                class="buttonCreate"
-                @click="createVariants">Добавить</el-button>
-              <el-button
-                @click="onCancel"
-                class="buttonCancel">Отменить</el-button>
-            </div>
-          </el-form-item>
-        </el-form>
-      </div>
-    </section>
+    
   </div>
 </template>
 
@@ -78,6 +33,7 @@ export default {
   data() {
     return {
       cmms: [],
+      courses: [],
       currentCmm: '',
       chosen: false,
       amountOfVariants: '',
@@ -145,8 +101,10 @@ export default {
       const path = 'http://localhost:8080/courses'
       axios.get(path).then(
         res => {
-          console.log(res.data)
-          console.log(res.data.courses[0])
+          //console.log(res.data)
+          //console.log(res.data.courses[0])
+          console.log(res.data.courses)
+          this.courses = res.data.courses
           //this.courses = res.status
         },
         error => {
